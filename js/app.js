e8c7b3f9abf978dc0da6c6c95d84de5929b42771
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-24 10:32:32 +0100

    FIX text search empty return+ trim

--- js/app.js
+++ js/app.js
@@ -235,13 +235,255 @@ createApp({
     return {
       currentUser: 0,
       firstMessage: 0,
-      contacts: contacts,
+      contacts: [
+        {
+          name: 'Michele',
+          avatar: './img/elonmusk_afp.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Hai portato a spasso il cane?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Ricordati di stendere i panni',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 16:15:22',
+              message: 'Tutto fatto!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Fabio',
+          avatar: './img/warren-buffett.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '20/03/2020 16:30:00',
+              message: 'Ciao come stai?',
+              status: 'sent'
+            },
+            {
+              date: '20/03/2020 16:30:55',
+              message: 'Bene grazie! Stasera ci vediamo?',
+              status: 'received'
+            },
+            {
+              date: '20/03/2020 16:35:00',
+              message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+              status: 'sent'
+            }
+          ],
+        },
+        {
+          name: 'Samuele',
+          avatar: './img/zuckerberg-scaled.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '28/03/2020 10:10:40',
+              message: 'La Marianna va in campagna',
+              status: 'received'
+            },
+            {
+              date: '28/03/2020 10:20:10',
+              message: 'Sicuro di non aver sbagliato chat?',
+              status: 'sent'
+            },
+            {
+              date: '28/03/2020 16:15:22',
+              message: 'Ah scusa!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Alessandro B.',
+          avatar: './img/gates.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Lo sai che ha aperto una nuova pizzeria?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Si, ma preferirei andare al cinema',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Alessandro L.',
+          avatar: './img/zuckerberg-scaled.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ricordati di chiamare il nonno',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Va bene, stasera la sento',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Claudia',
+          avatar: './img/Steve_Jobs_Headshot_2010-CROP2.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ciao Claudia, hai novità?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Non ancora',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:51:00',
+              message: 'Nessuna nuova, buona nuova',
+              status: 'sent'
+            }
+          ],
+        },
+        {
+          name: 'Federico',
+          avatar: './img/gates.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Fai gli auguri a Martina che è il suo compleanno!',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Grazie per avermelo ricordato, le scrivo subito!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Davide',
+          avatar: './img/warren-buffett.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ciao, andiamo a mangiare la pizza stasera?',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:51:00',
+              message: 'OK!!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Claudia',
+          avatar: './img/Steve_Jobs_Headshot_2010-CROP2.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ciao Claudia, hai novità?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Non ancora',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:51:00',
+              message: 'Nessuna nuova, buona nuova',
+              status: 'sent'
+            }
+          ],
+        },
+        {
+          name: 'Federico',
+          avatar: './img/gates.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Fai gli auguri a Martina che è il suo compleanno!',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Grazie per avermelo ricordato, le scrivo subito!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Davide',
+          avatar: './img/warren-buffett.jpeg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ciao, andiamo a mangiare la pizza stasera?',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:51:00',
+              message: 'OK!!',
+              status: 'received'
+            }
+          ],
+        }
+      ],
       inputValue: '',
+      findValue: '',
       response: 'ok!',
+      utenteTrovato: null,
     }
 
   },
   methods: {
+    findPerson(findValue){
+      let nomeInserito = this.findValue.trim()
+      if (nomeInserito=== '') {
+        return
+      }
+      console.log(findValue)
+      this.contacts.forEach(contact => {
+        // console.log(contact.name)
+        if(contact.name !== findValue){
+          console.log('sono diversi', contact.name, findValue)
+          this.utenteTrovato = true
+        } else {
+          console.log('sono uguali', contact.name, findValue)
+          this.utenteTrovato = false
+        }
+      })
+    },
     setCurrentUser(current) {
       this.currentUser = current
       // console.log(this.setCurrentUser)
@@ -258,13 +500,13 @@ createApp({
         status: 'sent',
       }
       // console.log(newMessage)
-      this.contacts[this.currentUser].messages.push(newMessage)
+        this.contacts[this.currentUser].messages.push(newMessage)
       this.inputValue = ''
       // console.log('messaggio inviato:', messaggioInserito)
       // console.log(this.contacts)
     },
-    responseMessage(current) {
-      // console.log('funzione risposta partita')
+    responseMessage() {
+      console.log('funzione risposta partita')
       setTimeout(() => {
         let messaggio = this.response
         let standardMessage = {
@@ -275,7 +517,7 @@ createApp({
         // console.log(standardMessage)
           this.contacts[this.currentUser].messages.push(standardMessage)
         
-      }, 5000)
+      }, 1000)
 
     }
   }
